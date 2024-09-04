536ddec698ed990ba0149513bb7ecc22e3bb9c8d
adarsh745 <145919884+adarsh745@users.noreply.github.com>
2024-09-04 22:05:39 +0530

    Add files via upload

---
+++ hand cricket/app.js
@@ -0,0 +1,21 @@
+class adarsh
+{
+    work()
+    {
+        console.log("writing code ")
+    }
+    eat()
+    {
+        console.log("eating dinner")
+    }
+}
+
+class abhi extends adarsh
+{
+    dance()
+    {
+        console.log("dancing")
+    }
+}
+
+let man =new abhi();
